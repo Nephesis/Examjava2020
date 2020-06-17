a93c440663c87f29ca685607defc8895d2ae6dfb
utin0l <utin0l@debian>
2020-06-17 22:07:26 +0200

    3eme

--- Examjava2020/src/MyClass.java
+++ Examjava2020/src/MyClass.java
@@ -1,11 +1,35 @@
+import java.sql.*;
 
 public class MyClass {
 
 	public static void main(String[] args) {
-	 int x = 10;
-	 
-	 System.out.println("bonjour " + x + " gens");
+		String dbUrl = "jdbc:mariadb://localhost/classicmodels?user=classicmodels&password=classicmodels";
+		String dbClass = "org.mariadb.jdbc.Driver";
+		String query = "select employeeNumber, firstName, lastName FROM employees";
+		
+		
+	
+	try {
+		Class.forName(dbClass);
+		Connection con = DriverManager.getConnection (dbUrl);
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		while (rs.next()) {
+		int employeeNumber = rs.getInt("employeeNumber");
+		String firstName = rs.getString("firstName");
+		String lastName = rs.getString("lastName");
+		System.out.println("Employee ppr "+ employeeNumber+ " " + firstName + " " + lastName );
+		} //end while
+		stmt.close();
+		con.close();
+		} //end try
+		catch(ClassNotFoundException e) {
+		e.printStackTrace();
+		}
+		catch(SQLException e) {
+		e.printStackTrace();
+		}
 
-	}
+}
 
 }
